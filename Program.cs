e6dfc767125ce00b260ff64966905bbae82562f8
lorenzo.legato <lorenzo.legato@itiscuneo.eu>
2023-02-16 17:53:48 +0100

    creazione e test funzioni createNode e createRelationship

--- Program.cs
+++ Program.cs
@@ -32,8 +32,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-using var dbController = new Neo4jController();
-Node node1 = new(1, "Pippo", null);
-Task task = dbController.CreateNode(node1);
 
 app.Run();
