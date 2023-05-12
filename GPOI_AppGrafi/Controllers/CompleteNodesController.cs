769d9fab53554291ba81d1bfc5c7c1d2e0b863c8
alessandro.michelis <alessandro.michelis@itiscuneo.eu>
2023-05-12 08:19:06 +0200

    complete nodes

---
+++ GPOI_AppGrafi/Controllers/CompleteNodesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GPOI_AppGrafi.Controllers
+{
+    public class CompleteNodesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
