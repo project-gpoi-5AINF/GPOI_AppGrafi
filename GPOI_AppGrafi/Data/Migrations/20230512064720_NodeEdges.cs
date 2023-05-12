1b02e744d2a5ea8eea5a01bbfe45d8cb0e6523ea
lorenzoLegato2004 <lorenzo.legato@itiscuneo.eu>
2023-05-12 09:01:12 +0200

    connessione node al database tramite il context e la migrazione funzionante

---
+++ GPOI_AppGrafi/Data/Migrations/20230512064720_NodeEdges.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GPOI_AppGrafi.Data.Migrations
+{
+    public partial class NodeEdges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Node",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Desc = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Node", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NodeSQL",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descr = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tipo = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NodeSQL", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Edge",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NodoOrigineId = table.Column<int>(type: "int", nullable: false),
+                    NodoTermineId = table.Column<int>(type: "int", nullable: false),
+                    Descr = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Edge", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Edge_Node_NodoOrigineId",
+                        column: x => x.NodoOrigineId,
+                        principalTable: "Node",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Edge_Node_NodoTermineId",
+                        column: x => x.NodoTermineId,
+                        principalTable: "Node",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Edge_NodoOrigineId",
+                table: "Edge",
+                column: "NodoOrigineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Edge_NodoTermineId",
+                table: "Edge",
+                column: "NodoTermineId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Edge");
+
+            migrationBuilder.DropTable(
+                name: "NodeSQL");
+
+            migrationBuilder.DropTable(
+                name: "Node");
+        }
+    }
+}
