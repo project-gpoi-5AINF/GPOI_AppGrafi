47396e98bf46feb7d598de623f8bc035e0be4add
alessandro.michelis <alessandro.michelis@itiscuneo.eu>
2023-05-12 08:19:41 +0200

    Complete Nodes c+v

--- GPOI_AppGrafi/Controllers/CompleteNodesController.cs
+++ GPOI_AppGrafi/Controllers/CompleteNodesController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GPOI_AppGrafi.Data;
+using GPOI_AppGrafi.Models;
+using GPOI_AppGrafi.Controllers;
 
 namespace GPOI_AppGrafi.Controllers
 {
     public class CompleteNodesController : Controller
     {
-        public IActionResult Index()
+        private readonly GPOI_AppGrafiContext _context;
+
+        public CompleteNodesController(GPOI_AppGrafiContext context)
         {
-            return View();
+            _context = context;
         }
+
+        /*
+        public IActionResult ModificaNodo(int id, string name, string descr, int parentId)
+        {
+            var updateNode = _context.NodeSQL.FromSql("UPDATE dbo.Node SET Name = {name} AND Descr = {descr} WHERE " +
+                "id = {id}");
+
+            var updateEdge = "UPDATE dbo.Edges SET nodoTermine = {parentId} WHERE " +
+                "id = {id}";
+
+            Neo4jController neo4j = new Neo4jController();
+            Node node = new Node(id, name, descr);
+            neo4j.Edit(id, node);
+        }
+        */
     }
 }
